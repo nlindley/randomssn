3f2d08d7317e641fcd8dc43f6d8b25d1c9ded8bd
Nicholas Lindley <me@thisoneplace.com>
2015-04-15 14:08:00 -0500

    Use lab for running tests

--- test/ssn.js
+++ test/ssn.js
@@ -1,11 +1,14 @@
+var Lab = require('lab');
+var lab = exports.lab = Lab.script();
 var expect = require('chai').expect;
-var sinon = require('sinon');
+
 var SSN = require('../ssn');
 
-describe('SSN', function() {
-  describe('::generate', function() {
-    it('Should match the format ###-##-####', function() {
+lab.experiment('SSN', function() {
+  lab.experiment('::generate', function() {
+    lab.test('Should match the format ###-##-####', function(done) {
       expect(SSN.generate()).to.match(/^\d{3}-\d{2}-\d{4}$/);
+      done();
     });
   });
 });
